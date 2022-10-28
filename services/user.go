aac89b247363a4e67f61d175f9b8938cbb713841
unknown <andianugrahaqsa@gmail.com>
2022-10-28 11:40:01 +0800

    feat(services) add user service

---
+++ services/user.go
@@ -0,0 +1,40 @@
+package services
+
+import (
+	"mini-project/models"
+	"mini-project/repositories"
+)
+
+type UserService struct {
+	Repository repositories.UserRepository
+}
+
+func NewUserService() UserService {
+	return UserService{
+		Repository: &repositories.UserRepositoryImpl{},
+	}
+}
+
+func (cs *UserService) GetAll() []models.User {
+	return cs.Repository.GetAll()
+}
+
+func (cs *UserService) GetByID(id int) models.User {
+	return cs.Repository.GetByID(id)
+}
+
+func (cs *UserService) Register(userRequest models.UserRequest) models.User {
+	return cs.Repository.Register(userRequest)
+}
+
+func (cs *UserService) Login(userRequest models.UserRequest) models.User {
+	return cs.Repository.Login(userRequest)
+}
+
+func (cs *UserService) Update(id int, userRequest models.UserRequest) models.User {
+	return cs.Repository.Update(id, userRequest)
+}
+
+func (cs *UserService) Delete(id int) bool {
+	return cs.Repository.Delete(id)
+}
