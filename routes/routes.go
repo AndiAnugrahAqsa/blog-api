3f76a65becaecc097669c1ce0cdf768cf3cb3db0
unknown <andianugrahaqsa@gmail.com>
2022-10-28 14:20:57 +0800

    fix(routes) fix path of role POST

--- routes/routes.go
+++ routes/routes.go
@@ -7,11 +7,21 @@ import (
 )
 
 var roleController = controllers.NewRoleController()
+var userController = controllers.NewUserController()
 
 func RoutesInit(e *echo.Echo) {
 	e.GET("/roles", roleController.GetAll)
 	e.GET("/roles/:id", roleController.GetByID)
-	e.POST("/roles/:id", roleController.Create)
+	e.POST("/roles", roleController.Create)
 	e.PUT("/roles/:id", roleController.Update)
 	e.DELETE("/roles/:id", roleController.Delete)
+
+	e.GET("/users", userController.GetAll)
+	e.GET("/users/:id", userController.GetByID)
+	e.PUT("/roles/:id", userController.Update)
+	e.DELETE("/roles/:id", userController.Delete)
+
+	// user auth
+	e.POST("/register", userController.Register)
+	e.POST("/login", userController.Login)
 }
