9b9541ad53bdb238422356bb59e6894b828b52ba
unknown <andianugrahaqsa@gmail.com>
2022-10-30 10:57:28 +0800

    feat(routes) add comment routes

--- routes/routes.go
+++ routes/routes.go
@@ -10,6 +10,7 @@ import (
 )
 
 var categoryController = controllers.NewCategoryController()
+var commentController = controllers.NewCommentController()
 var roleController = controllers.NewRoleController()
 var userController = controllers.NewUserController()
 
@@ -32,6 +33,13 @@ func RoutesInit(e *echo.Echo) {
 	superUserPrivateRoutes.POST("/categories", categoryController.Create)
 	superUserPrivateRoutes.PUT("/categories/:id", categoryController.Update)
 	superUserPrivateRoutes.DELETE("/categories/:id", categoryController.Delete)
+
+	e.GET("/comments", commentController.GetAll)
+	e.GET("/comments/:id", commentController.GetByID)
+	e.GET("comments/blog/:blog_id", commentController.GetByBlogID)
+	userPrivateRoutes.POST("/comments", commentController.Create)
+	userPrivateRoutes.PUT("/comments/:id", commentController.Update)
+	userPrivateRoutes.DELETE("/comments/:id", commentController.Delete)
 
 	superUserPrivateRoutes.GET("/roles", roleController.GetAll)
 	superUserPrivateRoutes.GET("/roles/:id", roleController.GetByID)
